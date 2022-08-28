55e15f1f2a7df7b815ae9be7b9521fdd8d876e88
Jongchan Ko <godol811@naver.com>
2022-08-28 15:50:49 +0900

    2022 8 27 - OneToMany Uni-direction
    1. OneToMany uni는 하나의 연결된 One 쪽의 테이블이 삭제되면 연결된 테이블들은 다 삭제 된다.
    
    2. 설정은 OneToMany 의 Cascade를 All 설정을 해주고
    Fetch는 하나를 검색할때 부를 수 있도록 Lazy를 설정 해주면된다.
    ex) @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)

---
+++ src/main/java/com/godol/hibernate/OneToManyUni/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,53 @@
+package com.godol.hibernate.OneToManyUni;
+
+import com.godol.hibernate.OneToManyUni.entity.Course;
+import com.godol.hibernate.OneToManyUni.entity.Instructor;
+import com.godol.hibernate.OneToManyUni.entity.InstructorDetail;
+import com.godol.hibernate.OneToManyUni.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+        // creat session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try{
+            session.beginTransaction();
+
+            int theId = 11;
+
+            Course tempCourse = session.get(Course.class, theId);
+
+
+
+            System.out.println("course : " + tempCourse);
+
+            System.out.println("reviews : " + tempCourse.getReviews());
+
+            session.delete(tempCourse);
+
+            // commit transaction
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            // add clean up code
+            session.close();
+            factory.close();
+
+        }
+
+
+    }
+}
